da17306900c42eb9c5e5a64f837e5dc8ef7dca42
Jody LeCompte <jody@jodylecompte.com>
2017-05-06 21:23:47 -0500

    Mock up of board index + a few deletions that got missed in git

--- app/views/layouts/index.volt.compiled
+++ app/views/layouts/index.volt.compiled
@@ -5,8 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-    <?=$this->getContent();?>
+    <header>
+        <div class="navbar navbar-default " role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header"><a class="navbar-brand" href="#">NovaBoard</a>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse navbar-menubuilder">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li><a href="/">Home</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#">Login</a>
+                        </li>
+                        <li>
+                            <a href="#">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </header>
+    <div class="container">
+        <?=$this->getContent();?>
+    </div>
+    <footer class="text-center">
+        Powered by NovaBoard 0.1 Alpha
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,112 @@
 <?php
-require_once(__DIR__ . '/../src/bootstrap.php')
-?>
+/**
+ * NovaBoard
+ * PHP Bulletin Board Software Written By Jody LeCompte
+ * License: MIT
+ * --
+ * URL: http://jodylecompte.com
+ * Github: https://github.com/jodylecompte/jodylecompte.com
+ *
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\View\Engine\Volt;
+try {
+    // Initialize the loader and set default directories
+    $loader = new Loader();
+    $loader->registerDirs(
+        [
+            "../app/controllers/",
+            "../app/models/",
+        ]
+    );
+    $loader->register();
+
+    //Initialize the DI and import system configuration
+    $di = new FactoryDefault();
+    $conf = require('../app/config/config.php');
+
+    $di->setShared('conf', function() use ($conf) {
+        return $conf;
+    });
+
+    // Connect to the database 
+    $di->set('db', function () {
+        $conf = $this->get('conf');
+        return new DbAdapter(array(
+            'host'     => $conf->dbHost,
+            'username' => $conf->dbUser,
+            'password' => $conf->dbPass,
+            'dbname'   => $conf->dbName
+        ));
+    });
+
+    // Load view system and Volt engine
+    $di->set(
+        "voltService",
+        function ($view, $di) {
+            $volt = new Volt($view, $di);
+
+            $volt->setOptions(
+                [
+                    "compiledExtension" => ".compiled",
+                    'stat' => true,
+                    'compileAlways' => true
+                ]
+            );
+
+            return $volt;
+        }
+    );
+
+    $di->set(
+        "view",
+        function () {
+            $view = new View();
+
+            $view->setViewsDir("../app/views/");
+            $view->registerEngines(
+                [
+                    ".volt" => "voltService",
+                ]
+            );
+
+            return $view;
+        }
+    );
+
+    // Setup the URI helpers and base URL
+    $di->set(
+        "url",
+        function () {
+            $conf = $this->get('conf');
+            $url = new UrlProvider();
+
+            $url->setBaseUri($conf->rootURL);
+
+            return $url;
+        }
+    );
+
+    // Initialise user sessions
+    $di->setShared('session', function() {
+        $session = new \Phalcon\Session\Adapter\Files();
+        $session->start();
+        return $session;
+    });
+
+    // Its show time
+    $application = new Application($di);
+    $response = $application->handle();
+    $response->send();
+} catch (\Exception $e) {
+    echo "Exception: ", $e->getMessage();
+}
