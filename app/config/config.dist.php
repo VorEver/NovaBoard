067e39444b8fbf932f0659d3b0a7e904f7d1cab6
Jody LeCompte <jody@jodylecompte.com>
2017-05-06 19:47:18 -0500

    Back on track with Phalcon

---
+++ app/config/config.dist.php
@@ -0,0 +1,9 @@
+<?php
+return new \Phalcon\Config(array(
+        'dbHost'   => 'localhost',
+        'dbUser'   => 'root',
+        'dbPass'   => '',
+        'dbName'   => '',
+        'rootURL'  => 'http://localhost/NovaBoard/'
+    )
+);
